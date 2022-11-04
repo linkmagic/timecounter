245d8b6a6ca6b9a9afe75bf09c2f2d92a94f46cd
Yevhen Sapsai <linkmagic@gmail.com>
2022-11-04 20:15:55 +0200

    calc time

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let totalSeconds = 333, days = 0, hours = 0, minutes = 0, seconds = 0;
+let totalSeconds = 0, days = 0, hours = 0, minutes = 0, seconds = 0;
+let timeCounterTimer = undefined;
+let idTotalDays, idTotalHours, idTotalMinutes, idTotalSeconds;
 
 function loadTime() {
   totalSeconds = window.localStorage.getItem('totalSeconds');
@@ -6,28 +8,48 @@ function loadTime() {
 function saveTime() {
   window.localStorage.setItem('totalSeconds', totalSeconds);
 }
-
+function initElements() {
+  idTotalDays = document.getElementById('idTotalDays');
+  idTotalHours = document.getElementById('idTotalHours');
+  idTotalMinutes = document.getElementById('idTotalMinutes');
+  idTotalSeconds = document.getElementById('idTotalSeconds');
+}
+function addLeadingZero(value) {
+  return (value > 9 ? '' : '0' ) + value;
+}
+function calcAndPrintTime() {
+  days = Math.floor(totalSeconds / 3600 / 24);
+  hours = Math.floor((totalSeconds - (days * 86400)) / 3600);
+  minutes = Math.floor((totalSeconds - (days * 86400) - (hours * 3600)) / 60);
+  seconds = (totalSeconds - (days * 86400) - (hours * 3600)) - (minutes * 60);
+  idTotalDays.textContent = addLeadingZero(days);
+  idTotalHours.textContent = addLeadingZero(hours);
+  idTotalMinutes.textContent = addLeadingZero(minutes);
+  idTotalSeconds.textContent = addLeadingZero(seconds);
+}
 function clickStart() {
-  console.log('start');
+  timeCounterTimer = setInterval(function() {
+      totalSeconds++;
+      calcAndPrintTime();
+    }, 1000);
 }
 function clickPause() {
-  console.log('Pause');
-}
-function clickSave() {
-  console.log('Save');
+  clearInterval(timeCounterTimer);
+  saveTime();
 }
 function clickReset() {
-  console.log('Reset');
+  if (confirm("Reset timer to 00:00:00:00 ?") == true) {
+    totalSeconds = 0;
+    days = 0;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    calcAndPrintTime();
+    saveTime();
+  }  
 }
 
-let divTotalTime = document.getElementById('idtotaltime');
-
-
-// let timeCounterTimer = setInterval(function() {
-//   seconds++;
-//   divTotalTime.textContent = seconds;
-// }, 1000);
-
-// saveTime();
-// loadTime();
-// divTotalTime.textContent = totalSeconds;
+// Run
+loadTime();
+initElements();
+calcAndPrintTime();
